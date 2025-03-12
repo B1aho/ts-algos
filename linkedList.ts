bd497f3cf676668d3ed2df93109396e26a38f98e
B1aho <wheelsurgut00@gmail.com>
2025-03-12 12:56:39 +0300

    Add find and contains methods

--- linkedList.ts
+++ linkedList.ts
@@ -29,20 +29,17 @@ class LinkedList implements ILinkedList {
     #head: IListNode | null = null;
     #tail: IListNode | null = null;
 
-    traverse(from: IListNode | null, to: IListNode | null) {
-        let total = 0;
-        if (from === null)
-            return total;
-        total++;
-        while (from && to && from !== to) {
-            from = from.next;
+
+    size() {
+        if (this.#head === null || this.#tail === null)
+            return 0;
+        let total = 1;
+        let ptr: IListNode | null = this.#head
+        while (ptr && ptr !== this.#tail) {
+            ptr = ptr.next;
             total++
         }
         return total;
-    }
-
-    size() {
-        return this.traverse(this.#head, this.#tail);
     }
 
     append(val: string) {
@@ -58,7 +55,7 @@ class LinkedList implements ILinkedList {
 
     prepend(val: string) {
         const node = new ListNode(val);
-        if (this.#head === null) {
+        if (this.size() === 0) {
             this.#head = node;
             this.#tail = node;
         } else {
@@ -81,21 +78,69 @@ class LinkedList implements ILinkedList {
     };
 
     at(idx: number) {
+        if (this.#head === null || idx <= 0)
+            return null;
         const node = new ListNode("");
-        return node;
+        let curr = 0;
+        let ptr: IListNode | null = this.#head;
+        while (ptr && curr !== idx) {
+            ptr = ptr.next;
+            curr++;
+        }
+        return ptr;
     };
+
     pop() {
-        return "";
+        if (this.size() <= 0) {
+            return null;
+        }
+        let data = null;
+        if (this.#tail && this.#tail === this.#head) {
+            data = this.#tail.value;
+            this.#head = null;
+            this.#tail = null;
+            return data;
+        } else {
+            let ptr = this.#head as IListNode;
+            while (ptr.next && ptr.next !== this.#tail)
+                ptr = ptr.next;
+            data = (this.#tail) ? this.#tail.value : null;
+            this.#tail = ptr;
+            ptr.next = null;
+        }
+        return data;
     }
+
     contains(val: string) {
-        return true;
+        if (this.size() === 0)
+            return false;
+        let ptr = this.#head
+        while (ptr) {
+            if (ptr.value === val)
+                return true;
+            ptr = ptr.next;
+        }
+        return false;
     }
+
     find(val: string) {
+        if (this.size() === 0)
+            return null;
+        let ptr = this.#head;
+        let idx = 0;
+        while (ptr) {
+            if (ptr.value === val)
+                return idx;
+            ptr = ptr.next;
+            idx++;
+        }
         return null;
     }
+
     insertAt(value: string, idx: number) {
         return true;
     }
+
     removeAt(idx: number) {
         return null;
     };
@@ -112,3 +157,12 @@ list.append("snake");
 list.append("turtle");
 list.prepend("monkey");
 console.log(list.toString());
+console.log('cat idx:', list.find("cat"));
+console.log(list.size());
+console.log(list.pop());
+console.log(list.pop());
+console.log(list.pop());
+console.log(list.toString());
+console.log(list.size());
+console.log('Contains parrot:', list.contains('parrot'));
+console.log('Dog index:', list.find("dog"));
