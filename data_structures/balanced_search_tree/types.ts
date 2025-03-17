4882be7c352707496b2b6058cc5959b7ad6cf6e3
B1aho <wheelsurgut00@gmail.com>
2025-03-17 07:50:21 +0300

    Add typization for BST

---
+++ data_structures/balanced_search_tree/types.ts
@@ -0,0 +1,24 @@
+type ValueType = number | string;
+
+export interface INode<T extends ValueType> {
+    value: T;
+    leftChild: INode<T> | null;
+    rightChild: INode<T> | null;
+}
+
+export interface ITree<T extends ValueType> {
+    root: INode<T> | null;
+    buildBST: (input: T[]) => INode<T> | null;
+    printTree: () => void;
+    insert: (value: T) => boolean;
+    delete: (value: T) => boolean;
+    find: (value: T) => INode<T> | null;
+    levelOrder: (fn: (value: T) => void) => void;
+    inOrder: (fn: (value: T) => void) => void;
+    preOrder: (fn: (value: T) => void) => void;
+    postOrder: (fn: (value: T) => void) => void;
+    height: (node: INode<T>) => number;
+    depth: (node: INode<T>) => number;
+    isBalanced: () => boolean;
+    rebalance: () => void;
+}
